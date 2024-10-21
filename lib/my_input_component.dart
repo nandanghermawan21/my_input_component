475e23e6945a6bebc24bb0e5d9090cd22874a4bd
Renoldi <renoldiboz@gmail.com>
2024-10-21 13:05:11 +0700

    add trim

--- lib/my_input_component.dart
+++ lib/my_input_component.dart
@@ -304,7 +304,7 @@ class InputComponentState<T> extends State<InputComponent<T>> {
                 0.0;
             widget.onChanged(parsedValue as T);
           } else if (T == String) {
-            widget.onChanged(value as T);
+            widget.onChanged(value.trim() as T);
           }
         });
       },
@@ -696,7 +696,7 @@ class Notifier<T> extends ChangeNotifier {
 
   TextEditingController controller = TextEditingController();
   set text(String value) {
-    controller.text = value;
+    controller.text = value.trim();
     commit();
   }
 }
